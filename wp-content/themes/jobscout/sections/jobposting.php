c9c41b500dceb226ac9f185b2b9c764503636bf2
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2023-11-20 21:44:48 +0700

    done Job Posting

--- wp-content/themes/jobscout/sections/jobposting.php
+++ wp-content/themes/jobscout/sections/jobposting.php
@@ -8,21 +8,28 @@
 $job_title         = get_theme_mod( 'job_posting_section_title', __( 'Job Posting', 'jobscout' ) );
 $ed_jobposting     = get_theme_mod( 'ed_jobposting', true );
 $count_posts       = wp_count_posts('job_listing'); 
+
 if ( $ed_jobposting && jobscout_is_wp_job_manager_activated() && $job_title  ) {
     ?>
-    <section id="job-posting-section" class="top-job-section">
-        <div class="container">
-            <?php 
-                if( $job_title ) echo '<h2 class="section-title">'. esc_html( $job_title ) .'</h2>'; 
-                if( jobscout_is_wp_job_manager_activated() && $count_posts->publish != 0 ){ ?>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <?php echo do_shortcode('[jobs show_filters="false" post_status="publish"]'); ?>
-                        </div>
-                    </div>
-                <?php } 
-            ?>
+<div id="content" class="site-content">
+    <div class="container">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <article class="page type-page status-publish hentry">
+                    <?php 
+                        if( $job_title ) echo '<header class="entry-header">
+                            <h2 class="entry-title" style="text-align: center;">'. esc_html( $job_title ) .'</h2>
+                        </header>'; 
+                        if( jobscout_is_wp_job_manager_activated() && $count_posts->publish != 0 ){ ?>
+                            <div class="entry-content" itemprop="text">
+                                <?php echo do_shortcode('[jobs show_filters="false" post_status="publish" per_page="6"]'); ?>
+                            </div>
+                        <?php } 
+                    ?>
+                </article>
+            </main>
         </div>
-    </section>
+    </div>
+</div>
     <?php
 }
