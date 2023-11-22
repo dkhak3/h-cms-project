db5f5de9a6502e9d980bdc1209f7cbe54cd90130
Minh Tây Phạm <117957414+MinhTayPP@users.noreply.github.com>
2023-11-22 08:30:20 +0700

    update banner news

--- wp-content/themes/jobscout/index.php
+++ wp-content/themes/jobscout/index.php
@@ -12,53 +12,55 @@
  * @package JobScout
  */
 
+
 get_header(); ?>
+<div id="primary" class="content-area">
 
-	<div id="primary" class="content-area">
-		
-        <?php 
-        /**
-         * Before Posts hook
-        */
-        do_action( 'jobscout_before_posts_content' );
-        ?>
-        
-        <main id="main" class="site-main">
-			<div class="article-section">
-				<div class="article-wrap">
-		<?php
-		if ( have_posts() ) :
+    <?php
+	/**
+	 * Before Posts hook
+	 */
+	do_action('jobscout_before_posts_content');
+	?>
+    <h2 class="section-title" style="letter-spacing: 2px;font-weight:700;text-align: center;">NEWEST BLOG ENTRIES
+    </h2>
+    <main id="main" class="site-main">
+        <div class="article-section">
+            <div class="article-wrap">
+                <?php
+				if (have_posts()):
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					/* Start the Loop */
+					while (have_posts()):
+						the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part('template-parts/content', get_post_format());
 
-			endwhile;
+					endwhile;
 
-		else :
+				else:
 
-			get_template_part( 'template-parts/content', 'none' );
+					get_template_part('template-parts/content', 'none');
 
-		endif; ?>
-				</div>
-			</div>
-		</main><!-- #main -->
-        
-        <?php
-        /**
-         * After Posts hook
-         * @hooked jobscout_navigation - 15
-        */
-        do_action( 'jobscout_after_posts_content' );
-        ?>
-        
-	</div><!-- #primary -->
+				endif; ?>
+            </div>
+        </div>
+    </main><!-- #main -->
+
+    <?php
+	/**
+	 * After Posts hook
+	 * @hooked jobscout_navigation - 15
+	 */
+	do_action('jobscout_after_posts_content');
+	?>
+
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
--- wp-content/themes/jobscout/sections/banner.php
+++ wp-content/themes/jobscout/sections/banner.php
@@ -1,39 +1,112 @@
-<?php
-/**
- * Banner Section
- * 
- * @package JobScout
- */
+<div id="banner-section" class="site-banner">
+    <div class="item">
+        <div id="wp-custom-header" class="wp-custom-header"><img
+                src="http://localhost/h-cms-project/wp-content/themes/jobscout/images/banner-image.jpg" width="1920"
+                height="704" alt="" decoding="async"></div>
+        <div class="banner-caption">
+            <div class="container">
+                <div class="caption-inner">
+                    <h2 class="title">Aim Higher, Dream Bigger</h2>
+                    <div class="description">
+                        <p>Each month, more than 7 million JobScout turn to website in their search for work, making
+                            over 160,000 applications every day.</p>
+                    </div>
+                    <div class="form-wrap">
+                        <div class="search-filter-wrap">
+                            <link rel="stylesheet"
+                                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+                                rel="stylesheet"
+                                integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+                                crossorigin="anonymous">
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                                crossorigin="anonymous">
+                            </script>
+                            <div class="job_listings ">
 
-$ed_banner         = get_theme_mod( 'ed_banner_section', true );
-$banner_title      = get_theme_mod( 'banner_title', __( 'Aim Higher, Dream Bigger', 'jobscout' ) );
-$banner_subtitle   = get_theme_mod( 'banner_subtitle', __( 'Each month, more than 7 million JobScout turn to website in their search for work, making over 160,000 applications every day.', 'jobscout' ) );
-$find_a_job_link   = get_option( 'job_manager_jobs_page_id', 0 );
-        
-if( $ed_banner && has_custom_header() ){ ?>
-    <div id="banner-section" class="site-banner<?php if( has_header_video() ) echo esc_attr( ' video-banner' ); ?>">
-        <div class="item">
-            <?php the_custom_header_markup(); ?>
-            <div class="banner-caption">
-                <div class="container">
-                    <div class="caption-inner">
-                        <?php 
-                            if( $banner_title ) echo '<h2 class="title">' . esc_html( $banner_title ) . '</h2>';
-                            if( $banner_subtitle ) echo '<div class="description">' . wpautop( wp_kses_post( $banner_subtitle ) ) . '</div>';
-                        ?>
-                        <div class="form-wrap">
-                            <div class="search-filter-wrap">
-                            <?php 
-                                if ( jobscout_is_wp_job_manager_activated() ) { 
-                                    if( $find_a_job_link ){
-                                        get_template_part('template-parts/header','form');
-                                    }else{
-                                         get_search_form();
-                                    }
-                                }else{
-                                    get_search_form();
-                                }
-                            ?>
+                                <form class="jobscout_job_filters" method="GET" style="
+    transform: translateX(-10px);
+" action="http://localhost/h-cms-project/jobs-2">
+                                    <div class="search_jobs row" style="margin-left:10px">
+
+                                        <div class="search_keywords col-md-7 d-flex" style="margin-right: 20px;">
+
+                                            <svg class="svg-inline--fa fa-magnifying-glass" style="padding-top: 20px;
+    padding-bottom: 16px;
+    background: white;
+    padding-left: 10px;
+    font-size: 24px;
+    color: orange;" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="magnifying-glass" role="img"
+                                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+                                                data-fa-i2svg="">
+                                                <path fill="currentColor"
+                                                    d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z">
+                                                </path>
+                                            </svg>
+                                            <!-- <i class="fa fa-search" style="padding-top: 20px;
+    padding-bottom: 16px;
+    background: white;
+    padding-left: 10px;
+    font-size: 24px;
+    color: orange;"></i> Font Awesome fontawesome.com -->
+                                            <input type="text" id="search_keywords" name="search_keywords"
+                                                placeholder="Search for jobs, companies, skills">
+                                        </div>
+
+                                        <div class="search_location col-md-3 d-flex">
+                                            <svg class="svg-inline--fa fa-location-dot" style="padding-top: 20px;
+    padding-bottom: 16px;
+    background: white;
+    padding-left: 10px;
+    font-size: 24px;
+    color: orange; padding-right: 5px;" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="location-dot"
+                                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
+                                                data-fa-i2svg="">
+                                                <path fill="currentColor"
+                                                    d="M168.3 499.2C116.1 435 0 279.4 0 192C0 85.96 85.96 0 192 0C298 0 384 85.96 384 192C384 279.4 267 435 215.7 499.2C203.4 514.5 180.6 514.5 168.3 499.2H168.3zM192 256C227.3 256 256 227.3 256 192C256 156.7 227.3 128 192 128C156.7 128 128 156.7 128 192C128 227.3 156.7 256 192 256z">
+                                                </path>
+                                            </svg>
+                                            <!-- <i class="fa-solid fa-location-dot" style="padding-top: 20px;
+    padding-bottom: 16px;
+    background: white;
+    padding-left: 10px;
+    font-size: 24px;
+    color: orange; padding-right:5px">&gt;</i> Font Awesome fontawesome.com -->
+                                            <!-- <input type="text"  id="search_location" name="search_location" placeholder=""> -->
+
+                                            <select id="search_location" name="search_location">
+                                                <option value="">Khu vực</option>
+                                                <option value=" CA">
+                                                    CA </option>
+                                                <option value=" California">
+                                                    California </option>
+                                                <option value=" Czech Republic">
+                                                    Czech Republic </option>
+                                                <option value=" Nepal">
+                                                    Nepal </option>
+                                                <option value=" Poland">
+                                                    Poland </option>
+                                                <option value=" UK">
+                                                    UK </option>
+                                                <option value=" US">
+                                                    US </option>
+                                            </select>
+
+                                        </div>
+
+
+                                        <div class="search_submit col-md-2"
+                                            style="padding-left:0px;padding-right:0px;color:EA751E;    margin-left: 20px;">
+                                            <!-- <input type="submit" value="SEARCH JOB"> -->
+                                            <input type="button" value="SEARCH JOB" style="    background: #EA751E;
+    color: #fff;
+    border: 1px solid #EA751E;">
+                                        </div>
+
+                                    </div>
+                                </form>
+
                             </div>
                         </div>
                     </div>
@@ -41,5 +114,4 @@ if( $ed_banner && has_custom_header() ){ ?>
             </div>
         </div>
     </div>
-<?php
-}
+</div>
